8c69a6f27c4fd74d3fa605ae9a5f69f9e5a43455
Dani Mibashan <dmibashan@manas.com.ar>
2019-07-29 14:05:30 -0300

    make sider menu responsive for mobile
    It now open and closes with toggle and close buttons.

--- ui/src/scripts/05-sidr-menu.js
+++ ui/src/scripts/05-sidr-menu.js
@@ -2,68 +2,41 @@
   'use strict'
 
   var sidrToggle = document.querySelector('.sidr-toggle'),
-    sidrPanel = document.querySelector('#sidr')
+    sidrPanel = document.querySelector('#sidr'),
+    mainContent = document.querySelector('.main'),
+    closeMenuButton = document.querySelector('.close-menu-btn')
 
   var sidrCollapse = document.querySelectorAll('.collapsible')
 
-  var toolbar = document.createElement('div')
-  toolbar.classList.add('sidr-toolbar')
-
-  // var backButton = document.createElement('button')
-  // backButton.classList.add('sidr-back')
-  // toolbar.appendChild(backButton)
-
-  var closeButton = document.createElement('button')
-  closeButton.classList.add('sidr-close')
-  toolbar.appendChild(closeButton)
-
-  var title = document.createElement('div')
-  title.classList.add('sidr-title')
-  toolbar.appendChild(title)
-
-  sidrPanel.insertBefore(toolbar, sidrPanel.firstChild)
-
-  // find(document, '#sidr .menu__link + .menu').forEach(function (menu) {
-
-  //   var submenuOpen = document.createElement('button')
-  //   submenuOpen.classList.add('submenu__open')
-  //   menu.parentNode.insertBefore(submenuOpen, menu)
-
-  //   submenuOpen.addEventListener('click', function () {
-  //     sidrPanel.setAttribute('data-submenu', 'yes')
-  //     menu.setAttribute('data-state', 'open')
-  //   })
-  // })
-
   sidrToggle.addEventListener('click', function (e) {
-    sidrPanel.setAttribute('data-state', 'enabled')
-    e.stopPropagation()
+    openNav()
+    sidrPanel.classList.toggle('active')
   })
+
+  closeMenuButton.addEventListener('click', function (e) {
+    closeNav()
+    sidrPanel.classList.toggle('active')
+  })
+
+  function openNav() {
+    sidrPanel.style.minWidth = '100vw';
+    mainContent.style.display = 'none';
+  }
+
+  function closeNav() {
+    sidrPanel.style.minWidth = '0';
+    mainContent.style.display = 'block';
+  }
 
   sidrCollapse.forEach(function (o) {
     o.addEventListener('click', function (e) {
-      this.classList.toggle("active")
+      this.classList.toggle('active')
       var dropdownContent = this.nextElementSibling
-      if (dropdownContent.style.display === "none") {
-        dropdownContent.style.display = "block";
+      if (dropdownContent.style.display === 'none') {
+        dropdownContent.style.display = 'block';
       } else {
-        dropdownContent.style.display = "none";
+        dropdownContent.style.display = 'none';
       }
     })
   })
-
-  closeButton.addEventListener('click', function () {
-    sidrPanel.removeAttribute('data-state')
-  })
-
-  sidrPanel.addEventListener('click', function (e) {
-    e.stopPropagation()
-  })
-  window.addEventListener('click', function () {
-    sidrPanel.removeAttribute('data-state')
-  })
-
-  //function find(from, selector) {
-  //  return [].slice.call(from.querySelectorAll(selector))
-  //}
 })()
