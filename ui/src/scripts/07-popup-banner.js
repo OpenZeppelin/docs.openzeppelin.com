213477d3628c88fa027b9590c25e4b02e4b56df1
Francisco Giordano <frangio.1@gmail.com>
2020-10-26 14:08:39 -0300

    Add popup banner (#249)
    Co-authored-by: davidsteinrueck <65513125+davidsteinrueck@users.noreply.github.com>

---
+++ ui/src/scripts/07-popup-banner.js
@@ -0,0 +1,34 @@
+;(function () {
+  const cookieName = 'popup_banner_closed';
+
+  function setClosed(value) {
+    document.cookie = `${cookieName}=${value};path=/;max-age=31536000`;
+  }
+
+  function getClosed() {
+    const cookie = document.cookie.split('; ').find(c => c.startsWith(`${cookieName}=`));
+    return cookie && cookie.split('=')[1];
+  }
+
+  if (getClosed()) {
+    return;
+  }
+
+  window.addEventListener('load', () => {
+    setTimeout(() => {
+      const banner = document.createElement('div');
+      banner.classList.add('popup-banner');
+      banner.innerHTML = `
+        <div class="popup-banner-text popup-banner-new">Automate your Ethereum operations with OpenZeppelin Defender — <a href="https://openzeppelin.com/defender">Learn More</a></div>
+        <button class="popup-banner-close" aria-label="Close" title="Close"></button>
+      `;
+
+      banner.querySelector('.popup-banner-close').addEventListener('click', e => {
+        setClosed(1);
+        e.currentTarget.parentElement.remove();
+      });
+
+      document.body.appendChild(banner);
+    }, 5000);
+  });
+})();
