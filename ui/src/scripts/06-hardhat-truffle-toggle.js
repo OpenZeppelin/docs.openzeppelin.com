3f37688888aeb0f9227f8b8b1de93b3beb9fa38f
Francisco Giordano <frangio.1@gmail.com>
2021-03-03 18:21:31 -0300

    Allow setting Hardhat/Truffle preference via URL param

--- ui/src/scripts/06-hardhat-truffle-toggle.js
+++ ui/src/scripts/06-hardhat-truffle-toggle.js
@@ -9,21 +9,50 @@
   });
 
   const cookieName = 'hardhat_truffle_preference';
+  const paramName = 'pref';
   const defaultPreference = 'hardhat';
 
-  function getPreference() {
+  function getCookie() {
     const cookie = document.cookie.split('; ').find(c => c.startsWith(`${cookieName}=`));
-    const current = cookie && cookie.split('=')[1] || defaultPreference;
-    const other = current === 'hardhat' ? 'truffle' : 'hardhat';
-    return { current, other };
+    return cookie && cookie.split('=')[1];
+  }
+
+  function setCookie(preference) {
+    document.cookie = `${cookieName}=${preference};path=/;max-age=31536000`;
+  }
+
+  function getURLParam() {
+    return new URLSearchParams(document.location.search).get(paramName);
+  }
+
+  function setURLParam(preference) {
+    const url = new URL(document.location);
+    const params = new URLSearchParams(url.search);
+    if (params.has('pref')) {
+      params.set('pref', preference);
+      url.search = params.toString();
+      history.replaceState(null, '', [url]);
+    }
+  }
+
+  function getPreference() {
+    const current = getURLParam() || getCookie();
+    if (current === 'hardhat' || current === 'truffle') {
+      return current;
+    } else {
+      return defaultPreference;
+    }
   }
 
   function togglePreference() {
-    const { current, other } = getPreference();
+    const current = getPreference();
+    const other = current === 'hardhat' ? 'truffle' : 'hardhat';
     document.body.classList.replace(`preference-${current}`, `preference-${other}`);
-    document.cookie = `${cookieName}=${other};path=/;max-age=31536000`;
+    setCookie(other);
+    setURLParam(other);
   }
 
-  const { current } = getPreference();
+  const current = getPreference();
+  setCookie(current);
   document.body.classList.add(`preference-${current}`);
 })();
