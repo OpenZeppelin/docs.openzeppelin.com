6f1d5d3b4ba13007c658e08f758150eb7538aabe
Francisco Giordano <frangio.1@gmail.com>
2021-03-08 22:05:35 -0300

    clean up

--- ui/src/scripts/05-sidr-menu.js
+++ ui/src/scripts/05-sidr-menu.js
@@ -1,37 +1,24 @@
 (function () {
-  'use strict'
+  'use strict';
 
-  var sidrToggle = document.querySelector('.sidr-toggle'),
-    sidrPanel = document.querySelector('#sidr'),
-    mainContent = document.querySelector('.main'),
-    closeMenuButton = document.querySelector('.close-menu-btn')
-
+  const sidrToggle = document.querySelector('.sidr-toggle');
+  const sidrPanel = document.querySelector('#sidr');
+  const mainContent = document.querySelector('.main');
+  const closeMenuButton = document.querySelector('.close-menu-btn');
 
   sidrToggle.addEventListener('click', function (e) {
-    openNav()
-    sidrPanel.classList.toggle('active')
-  })
+    sidrPanel.classList.toggle('active');
+  });
 
   closeMenuButton.addEventListener('click', function (e) {
-    closeNav()
-    sidrPanel.classList.toggle('active')
-  })
+    sidrPanel.classList.toggle('active');
+  });
 
-  function openNav() {
-    sidrPanel.style.minWidth = '100vw';
-    mainContent.style.display = 'none';
-  }
-
-  function closeNav() {
-    sidrPanel.style.minWidth = '0';
-    mainContent.style.display = 'block';
-  }
-
-  var collapseToggles = document.querySelectorAll('.collapse-toggle')
+  const collapseToggles = document.querySelectorAll('.collapse-toggle');
 
   collapseToggles.forEach(function (o) {
     o.addEventListener('click', function (e) {
-      this.classList.toggle('active')
-    })
-  })
+      this.classList.toggle('active');
+    });
+  });
 })();
