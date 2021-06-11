44236016011dea4656cc7a586905b72099772f6d
Francisco Giordano <frangio.1@gmail.com>
2021-06-10 22:30:57 -0300

    add copy to clipboard and light code theme

---
+++ ui/src/scripts/07-copy-code.js
@@ -0,0 +1,25 @@
+(function () {
+  function onDOMContentLoaded(callback) {
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', callback);
+    } else {
+      callback();
+    }
+  }
+
+  function copyClick(e) {
+    const code = e.target.parentElement.innerText;
+    navigator.clipboard.writeText(code);
+  }
+
+  onDOMContentLoaded(() => {
+    for (const elem of document.querySelectorAll('.listingblock')) {
+      const btn = document.createElement('button');
+      btn.classList.add('btn-icon', 'btn-copy');
+      btn.setAttribute('aria-label', 'Copy');
+      btn.innerHTML = '<svg class="icon"><use href="#copy-icon"/></svg>';
+      btn.addEventListener('click', copyClick);
+      elem.prepend(btn);
+    }
+  });
+})();
