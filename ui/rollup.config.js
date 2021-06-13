6cb00c65ee4c6686cc06b442e15e98ab57d4030d
Francisco Giordano <frangio.1@gmail.com>
2021-06-13 18:50:04 -0300

    replace gulp with rollup + custom scripts

---
+++ ui/rollup.config.js
@@ -0,0 +1,40 @@
+import styles from 'rollup-plugin-styles';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+import { terser } from 'rollup-plugin-terser';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: 'src/index.js',
+  output: {
+    dir: 'theme/dist',
+    format: 'iife',
+    sourcemap: true,
+    assetFileNames: '[name][extname]',
+  },
+  plugins: [
+    replace({
+      preventAssignment: true,
+      include: '../**/node_modules/highlightjs-solidity/solidity.js',
+      delimiters: ['', ''],
+      'var module = module ? module : {};': '',
+    }),
+
+    styles({
+      mode: 'extract',
+      url: false,
+      minimize: production,
+      sourceMap: true,
+    }),
+
+    production && terser(),
+
+    resolve(),
+    commonjs(),
+  ],
+  watch: {
+    clearScreen: false,
+  },
+};
