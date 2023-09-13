0f79b5653a98cd794d692e1601c9be77ed4afacb
Julio A <52619625+julio-cfa@users.noreply.github.com>
2023-09-13 02:19:20 +0200

    Update custom_headers.sh

--- scripts/custom_headers.sh
+++ scripts/custom_headers.sh
@@ -5,7 +5,7 @@
 buildDir='build/site'
 
 input_text="/*
-  Content-Security-Policy: script-src 'self' *.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN js.hs-scripts.com js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
+  Content-Security-Policy: script-src 'self' www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN js.hs-scripts.com js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
 
 echo "$input_text" > "$buildDir/_headers"
 
