0b47591e8f2e19d9ce0fff31442e5f978d7b101c
Francisco <fg@frang.io>
2023-09-13 01:00:10 -0300

    Update custom_headers.sh

--- scripts/custom_headers.sh
+++ scripts/custom_headers.sh
@@ -2,11 +2,11 @@
 
 # This script will create a custom _headers file with security headers. Ref. for the CSP header: https://content-security-policy.com/examples/netlify/
 
-buildDir='build/site'
+build_dir='build/site'
 
 input_text="/*
   Content-Security-Policy: script-src 'self' 'unsafe-inline' wizard.openzeppelin.com www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN js.hs-scripts.com netlify-cdp-loader.netlify.app js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
 
-echo "$input_text" > "$buildDir/_headers"
+echo "$input_text" > "$build_dir/_headers"
 
 echo "Content-Security-Policy updated and saved to _headers file."
