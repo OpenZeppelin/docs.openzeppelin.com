ec3f221792d7869bb651b5787570570eb9089248
Francisco <fg@frang.io>
2023-09-22 18:19:32 -0300

    Fix MathJax (#383)

--- scripts/custom_headers.sh
+++ scripts/custom_headers.sh
@@ -4,8 +4,11 @@
 
 build_dir='build/site'
 
+# See ui/theme/partials/footer-scripts.hbs
+MATHJAX_SRC="'sha512-T8xxpazDtODy3WOP/c6hvQI2O9UPdARlDWE0CvH1Cfqc0TXZF6GZcEKL7tIR8VbfS/7s/J6C+VOqrD6hIo++vQ=='"
+
 input_text="/*
-  Content-Security-Policy: script-src 'self' 'unsafe-inline' wizard.openzeppelin.com www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN js.hs-scripts.com netlify-cdp-loader.netlify.app js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
+  Content-Security-Policy: script-src 'self' 'unsafe-inline' wizard.openzeppelin.com www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN $MATHJAX_SRC js.hs-scripts.com netlify-cdp-loader.netlify.app js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
 
 echo "$input_text" > "$build_dir/_headers"
 
