73cf6e19317312db8862b9416b2f9c06b429d0c7
ernestognw <ernestognw@gmail.com>
2023-11-01 13:41:56 -0600

    Configure scripts through CSP

--- scripts/custom_headers.sh
+++ scripts/custom_headers.sh
@@ -7,8 +7,12 @@ build_dir='build/site'
 # See ui/theme/partials/footer-scripts.hbs
 MATHJAX_SRC="'sha512-T8xxpazDtODy3WOP/c6hvQI2O9UPdARlDWE0CvH1Cfqc0TXZF6GZcEKL7tIR8VbfS/7s/J6C+VOqrD6hIo++vQ=='"
 
+# See ui/theme/partials/head.hbs
+GOOGLE_ANALYTICS_SRC="'sha256-HaiMAvIsR7hNb9URp31XK/UjtXaDF/bxnO2EaAE3AVw='"
+GOOGLE_TAG_MANAGER_SRC="'sha256-3wLxYvNYHedBnOMFM9cV6jwtauamPHbrG+ixW+C6Ne0='"
+
 input_text="/*
-  Content-Security-Policy: script-src 'self' 'unsafe-inline' wizard.openzeppelin.com www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN $MATHJAX_SRC js.hs-scripts.com netlify-cdp-loader.netlify.app js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
+  Content-Security-Policy: script-src 'self' 'unsafe-inline' wizard.openzeppelin.com www.googletagmanager.com $NETLIFY_IMAGES_CDN_DOMAIN $MATHJAX_SRC $GOOGLE_ANALYTICS_SRC $GOOGLE_TAG_MANAGER_SRC js.hs-scripts.com netlify-cdp-loader.netlify.app js.hsadspixel.net js.hsleadflows.net js.hs-analytics.net js.hubspot.com js.headleadflows.net js.hscollectedforms.net js.hs-banner.com no-cache.hubspot.com *.hs-sites.com static.hsappstatic.net *.usemessages.com *.hubspotusercontent00.net *.hubspot.net play.hubspotvideo.com cdn2.hubspot.net *.hscollectedforms.net *.hsleadflows.net *.hsforms.net *.hsforms.com *.hs-scripts.com *.hubspotfeedback.com feedback.hubapi.com;"
 
 echo "$input_text" > "$build_dir/_headers"
 
--- ui/src/js/version-selector.js
+++ ui/src/js/version-selector.js
@@ -1,6 +1,6 @@
-window.onload = function () {
+window.addEventListener("load", function () {
   const versionBtn = document.getElementById("version-selector");
   versionBtn.addEventListener("click", function () {
     versionBtn.focus();
   });
-};
+});
